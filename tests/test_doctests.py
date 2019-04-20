d98e1664455b9c75c8250f0146ff1a577977eb91
masterAndrey <filbert@yandex.ru>
2019-04-20 21:12:48 +0300

    doctests auto addition to unittests suite 2

--- tests/test_doctests.py
+++ tests/test_doctests.py
@@ -1,16 +1,19 @@
 """
-Add doctests to unittests suite, so you have one nice reports and one test result (exit code).
+doctests from all modules of packages listed in PACKAGES
+will be added to unit tests suite.
 
-To add doctests you should import all packages with doctests and list them in doctest_packages
+So you will have one nice test report and one test result/exit code in CI.
+
+You should import below all your packages with doctests and list them in PACKAGES.
 """
 import doctest
 from os.path import dirname, basename, isfile
 from glob import glob
 from importlib import import_module
+
 import tests
 import bombard
-
-doctests_packages = [bombard, tests]
+PACKAGES = [bombard, tests]
 
 PY_EXT = '.py'
 
@@ -20,7 +23,7 @@ def load_tests(loader, tests, ignore):
     Unittest hook to add tests to auto-discovered ones
     https://docs.python.org/3/library/unittest.html#load-tests-protocol
     """
-    for package in doctests_packages:
+    for package in PACKAGES:
         for module_file in glob(dirname(package.__file__) + f'/*{PY_EXT}'):
             if isfile(module_file) and not module_file.endswith('__init__.py'):
                 tests.addTest(doctest.DocTestSuite(
