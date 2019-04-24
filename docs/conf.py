aca0400c479a61dbca79e9868fe2a5d7a66756ca
Andrei Sorokin2 <Andrei_Sorokin2@epam.com>
2019-04-24 13:29:18 +0300

    locale dirs in doc config 2

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ release = bombard.__version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+#language = None
 
 import os.path
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
