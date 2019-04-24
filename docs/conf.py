aa1d23b567b6014294154f686b76b8ee6a74e2bc
Andrei Sorokin2 <Andrei_Sorokin2@epam.com>
2019-04-24 13:25:55 +0300

    locale dirs in doc config

--- docs/conf.py
+++ docs/conf.py
@@ -69,6 +69,11 @@ release = bombard.__version__
 # Usually you set "language" from the command line for these cases.
 language = None
 
+import os.path
+BASEDIR = os.path.dirname(os.path.abspath(__file__))
+locale_dirs = [os.path.join(BASEDIR, 'locale/')]
+gettext_compact = False
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
