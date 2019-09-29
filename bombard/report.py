11433cc6b9d04de6e6c4d2ff09a6bde876046a3c
andgineer <filbert@yandex.ru>
2019-09-29 16:13:54 +0300

    report different types for different dimensions

--- bombard/report.py
+++ bombard/report.py
@@ -13,19 +13,27 @@ from bombard.terminal_colours import red
 from copy import deepcopy
 
 
-ARRAY_UINT64 = 'Q'
+ARRAY_UINT64 = 'Q'  # in 8 bytes 200 000 days (we store time in ns). 4 bytes is only 3 days.
+ARRAY_UINT32 = 'L'  # sizes up to 4 Gbytes
 SUCCESS_GROUP = 'success'
 FAIL_GROUP = 'fail'
 GROUPS = [SUCCESS_GROUP, FAIL_GROUP]
-DIMENSIONS = ['time', 'size']
+TIME = 'time'
+SIZE = 'size'
+DIMENSIONS = {
+    TIME: {'type': ARRAY_UINT64},
+    SIZE: {'type': ARRAY_UINT32},
+}
 STAT_DEFAULT = {
-    name: array(ARRAY_UINT64) for name in DIMENSIONS
+    name: array(params['type']) for name, params in DIMENSIONS.items()
 }
 
 
 class Reporter:
     """
     Report bombard's result
+
+    Access to stat data only by log(), filter(), reduce()
     """
     def __init__(
             self,
@@ -45,6 +53,12 @@ class Reporter:
         self.ok = success_statuses
 
         self.stat = {}  # stat[request type][status]
+        # todo implement cache
+        # To select the best approach cache compare benchmarks for two versions:
+        #  1) fill cache in log() and use in filter()/reduce()
+        #  2) fill cache in filter() and use in reduce()
+        # self.stat_by_group = {}  # cache
+        # self.stat_by_request = {}  # cache
 
     def group_name_by_status(self, status):
         """
@@ -65,8 +79,8 @@ class Reporter:
         :param response_size: Response body size
         """
         self.stat.setdefault(request_name, {}).setdefault(status, deepcopy(STAT_DEFAULT))
-        self.stat[request_name][status]['time'].append(elapsed)
-        self.stat[request_name][status]['size'].append(response_size)
+        self.stat[request_name][status][TIME].append(elapsed)
+        self.stat[request_name][status][SIZE].append(response_size)
 
     @property
     def total_elapsed_ns(self):
@@ -75,35 +89,33 @@ class Reporter:
     def reduce(
             self,
             reduce_func,
+            dimension_name: str,
             status_group_filter: str = None,
             request_name_filter: str = None
     ) -> dict:
         """
-        Reduce by group and/or request_name with the reduce_func
+        Reduce the dimension by group and/or request_name with the reduce_func
         Returns dict {'time':, 'size':}
         """
-        result = {
-            name: 0 for name in DIMENSIONS
-        }
+        result = 0
         for request_name in self.stat:
             if request_name == request_name_filter or request_name_filter is None:
                 for status in self.stat[request_name]:
                     if self.group_name_by_status(status) == status_group_filter or status_group_filter is None:
-                        for dimension in DIMENSIONS:
-                            result[dimension] += reduce_func(self.stat[request_name][status][dimension])
+                        result += reduce_func(self.stat[request_name][status][dimension_name])
         return result
 
-    def filter_dimension(
+    def filter(
             self,
             dimension_name: str,
             status_group_filter: str = None,
             request_name_filter: str = None
     ) -> array:
         """
-        Filter by group and/or request_name,
+        Filter the dimension by group and/or request_name,
         Returns array with values from dimention_name
         """
-        dimension = array(ARRAY_UINT64)
+        dimension = array(DIMENSIONS[dimension_name]['type'])
         for request_name in self.stat:
             if request_name == request_name_filter or request_name_filter is None:
                 for status in self.stat[request_name]:
@@ -132,12 +144,12 @@ class Reporter:
         """
         result = []
         for dimension_name in DIMENSIONS:
-            dimension = self.filter_dimension(dimension_name, status_group_filter, request_name_filter)
+            dimension = self.filter(dimension_name, status_group_filter, request_name_filter)
             if dimension:
                 result.append(
                     self.dimension_stat_report(
                         dimension,
-                        self.pretty_ns if 'time' in dimension_name else pretty_sz
+                        self.pretty_ns if TIME in dimension_name else pretty_sz
                     )
                 )
         if not result:
@@ -145,7 +157,7 @@ class Reporter:
         return '\n'.join(result)
 
     def statuses_report(self, request_name_filter: str = None) -> str:
-        return ', '.join([f'{group} {self.reduce(len, group, request_name_filter)["time"]}'
+        return ', '.join([f'{group} {self.reduce(len, TIME, group, request_name_filter)}'
                           for group in GROUPS])
 
     def pretty_ns(self, elapsed_ns: int):
@@ -156,10 +168,9 @@ class Reporter:
             return result
 
     def report(self):
-        total_sum = self.reduce(sum)
-        size_sum = total_sum['size']
-        total_ns = total_sum['time']
-        total_num = self.reduce(len)['time']
+        size_sum = self.reduce(sum, SIZE)
+        total_ns = self.reduce(sum, TIME)
+        total_num = self.reduce(len, TIME)
         elapsed_sec = total_ns / (10 ** 9)
         total_line = ' '.join([
             f'Got `{total_num}` responses',
@@ -170,7 +181,7 @@ class Reporter:
         ])
         by_group = []
         for status_group in GROUPS:
-            by_group.append(f'''#### {status_group}: {self.reduce(len, status_group)['time']} 
+            by_group.append(f'''#### {status_group}: {self.reduce(len, TIME, status_group)} 
 {self.filtered_report(status_group)}
 ''')
         by_group = '\n'.join(by_group)
