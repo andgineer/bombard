72828db8f653e6cd7858618b77c443faf48ddee3
andgineer <filbert@yandex.ru>
2020-03-19 11:09:51 +0000

    install dependencies in container

--- install_local.sh
+++ install_local.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Install locally to test before uploading to PyPi
-sudo python3 -m pip install -r requirements.txt --ignore-installed
+python3 -m pip install -r requirements.txt --ignore-installed
 
 rm -rf dist/*
 ./build.sh
 
-sudo python3 -m pip install dist/$(ls dist) --upgrade
+python3 -m pip install dist/$(ls dist) --upgrade
