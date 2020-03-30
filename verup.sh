638f8573f40bdea563010ed302fb272e49d680e4
andgineer <filbert@yandex.ru>
2020-03-30 12:51:29 +0300

    check if git is dirty before setting version tag

--- verup.sh
+++ verup.sh
@@ -19,6 +19,12 @@ fi
 
 echo -e "Last version tag: \033[33m$major.$minor.$build\033[39m"
 
+if [[ $(git diff-index HEAD) ]]; then
+        echo -e "\n\033[33mPlease commit and push all changes" \
+        "before setting version tag\033[39m\n"
+        exit -1
+fi
+
 if [[ "$1" == "release" ]]; then
     build=0
     minor=0
