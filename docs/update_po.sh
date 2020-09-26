3424d7e48098a9bd3002daee33f77209d735df97
andgineer <filbert@yandex.ru>
2020-09-26 08:17:17 +0300

    docs update script

---
+++ docs/update_po.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# refresh po-file so they are ready to push to weblate for example
+#
+
+# 1st create pot in _build/gettext
+make gettext
+
+# create sphinx locales
+sphinx-intl update -p _build/gettext -l ru_RU
