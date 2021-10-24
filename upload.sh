3af837083453a24ff6acd437f23c23dafe792722
andgineer <andrey@sorokin.engineer>
2021-10-24 14:25:49 +0300

    removed verup because in this project we use github releases

--- upload.sh
+++ upload.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # Uploads built package (see build.sh) to PyPi repo
 # Do not forget to test installation locally with install_local.sh
+#
+# This is manual upload.
+# The package will be uploaded automatically when you create new release on github
 rm -rf build/*
 rm -rf dist/*
 ./build.sh
